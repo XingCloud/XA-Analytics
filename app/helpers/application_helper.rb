2bd5e10b555002197a94dfe76640086146385428
yakjuly <yakjuly@gmail.com>
2012-03-15 17:00:43 +0800

    code metric.js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,13 +14,14 @@ module ApplicationHelper
     }
   end
   
-  def include_tempalate_js(*args)
+  def include_js_template(*args)
     filenames = args.map do |arg|
-      "/js_templates/#{arg}.jst.html"
+      "/js_templates/#{arg}.jst.haml"
     end
     
-    content_for(:head) { 
+    #content_for(:head) { 
       javascript_include_tag(*filenames)
-    }
+    #}
   end
+  
 end
