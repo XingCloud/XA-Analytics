82be603a72b618604e28257fa36c92050f604157
yakjuly <yakjuly@gmail.com>
2012-03-28 19:07:13 +0800

    ignore fb event

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+set :application, "analytic"
+set :repository,  "git@yakjuly.com:analytic.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "app@119.254.28.37"                          # Your HTTP server, Apache/etc
+role :app, "app@119.254.28.37"                      # This may be the same as your `Web` server
+role :db,  "app@119.254.28.37"    , :primary => true # This is where Rails migrations will run
+
+set :user, "app"
+set :use_sudo, false
+set :deploy_to, "/home/app/apps/analytic"
+
+set :branch, "master"
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :deploy_via, :remote_cache
+
+default_environment["PATH"] = "/sbin:/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/home/app/bin:/usr/local/sbin:/usr/sbin:"
+
+task :custom_symlink do
+  
+  %w(tmp log).each do |dir|
+    run "cd #{release_path} && rm -rf #{dir} && ln -sf #{shared_path}/#{dir} ."
+  end
+  
+  %w(database app_config).each do |config_file|
+    run "cd #{release_path} && rm -rf config/#{config_file}.yml && ln -sf #{shared_path}/config/#{config_file}.yml config/#{config_file}.yml"
+  end
+  
+end
+
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+before "deploy:finalize_update", "custom_symlink"
