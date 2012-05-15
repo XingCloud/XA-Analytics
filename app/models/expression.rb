effe5ce2e18ed1614708ebf4d7947fa44414a60a
方建 <superman601@126.com>
2012-05-15 18:35:52 +0800

    fix time bug

--- app/models/expression.rb
+++ app/models/expression.rb
@@ -6,21 +6,17 @@ class Expression < ActiveRecord::Base
 
   # 组装给后端的数据
   def to_hsh
-    if self.name.match("time")
-      {self.name => {"$handler" => "DateSplittor"}}
-    else
-      if self.operator.match('eq')
-        if self.value_type == 'int'
-          {self.name => self.value.to_i}
-        else
-          {self.name => self.value}
-        end
+    if self.operator.match('eq')
+      if self.value_type == 'int'
+        {self.name => self.value.to_i}
       else
-        if self.value_type == 'int'
-          {self.name => {"$#{self.operator}" => self.value.to_i}}
-        else
-          {self.name => {"$#{self.operator}" => self.value}}
-        end
+        {self.name => self.value}
+      end
+    else
+      if self.value_type == 'int'
+        {self.name => {"$#{self.operator}" => self.value.to_i}}
+      else
+        {self.name => {"$#{self.operator}" => self.value}}
       end
     end
   end
