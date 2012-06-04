c5cfea8792098d150f9125541686c66466502ea4
方建 <superman601@126.com>
2012-06-04 13:00:56 +0800

    fix bug for validate px py for nil

--- app/models/project_widget.rb
+++ app/models/project_widget.rb
@@ -4,8 +4,8 @@ class ProjectWidget < ActiveRecord::Base
 
   validates_presence_of :widget_id, :project_id, :display
   validates_numericality_of :display, :only_integer => true, :greater_than_or_equal_to => 0
-  validates_numericality_of :px, :only_integer => true, :greater_than_or_equal_to => 0
-  validates_numericality_of :py, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :px, :only_integer => true, :greater_than_or_equal_to => 0, :if => proc{|m| m.px.present? }
+  validates_numericality_of :py, :only_integer => true, :greater_than_or_equal_to => 0, :if => proc{|m| m.py.present? }
 
   scope :visible, where(:display => 1)
 end
