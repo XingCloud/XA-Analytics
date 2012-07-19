89de0e91b3c407cbe013d9e5e1b7fea47332011f
方建 <superman601@126.com>
2012-07-19 17:57:36 +0800

    add monitor for analytic service Gemfile Gemfile.lock config/app_config.yml lib/analytic_service.rb

--- db/migrate/20120601071149_rename_join_tables.rb
+++
@@ -1,8 +0,0 @@
-class RenameJoinTables < ActiveRecord::Migration
-  def change
-    rename_table :widget_connectors, :project_widgets
-    rename_table :metrics_report_tabs, :report_tab_metrics
-    add_column :report_tab_metrics, :id, :primary_key
-    add_column :report_tab_metrics, :position, :integer, :default => 0
-  end
-end
