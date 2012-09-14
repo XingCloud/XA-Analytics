c70ac8d013d056723b3cd09c7e29a65d0109a9d8
方建 <superman601@126.com>
2012-09-14 20:33:51 +0800

    fix destroy bug

--- lib/workers/sync_user_attributes.rb
+++ lib/workers/sync_user_attributes.rb
@@ -5,8 +5,8 @@ module Workers
     def self.perform(project_id, action = "SAVE", user_attribute_id = nil)
       project = Project.find(project_id)
       if user_attribute_id.present?
-        user_attribute = UserAttribute.find(user_attribute_id).serialize
-        AnalyticService.sync_user_attribute(project, {:type => action, :params => [user_attribute].to_json})
+        user_attribute = UserAttribute.find(user_attribute_id)
+        AnalyticService.sync_user_attribute(project, {:type => action, :params => [user_attribute.serialize].to_json})
         if action == "REMOVE"
           raise "destroy error" unless user_attribute.destroy
         end
