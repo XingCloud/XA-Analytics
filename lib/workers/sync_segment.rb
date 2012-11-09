7cf1d4adfeedcea243649670a3bd7d5ea51ddc94
方建 <superman601@126.com>
2012-11-09 14:59:02 +0800

    fix bug for sync segment forget to_json

--- lib/workers/sync_segment.rb
+++ lib/workers/sync_segment.rb
@@ -4,7 +4,7 @@ module Workers
 
     def self.perform(segment_id, segment, project_id, action="APPEND_OR_UPDATE")
       project = (project_id.present? ? Project.find(project_id) : nil)
-      AnalyticService.sync_segments(action, {segment_id => segment}, project)
+      AnalyticService.sync_segments(action, {segment_id => segment.to_json}, project)
     end
   end
 end
