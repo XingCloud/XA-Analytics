8f74a786cdd620ab7dcd9e1bda87b1b861d05850
方建 <superman601@126.com>
2012-12-26 11:19:00 +0800

    do not validate presence

--- app/models/report_tab_metric.rb
+++ app/models/report_tab_metric.rb
@@ -2,6 +2,6 @@ class ReportTabMetric < ActiveRecord::Base
   belongs_to :report_tab
   belongs_to :metric
 
-  validates_presence_of :report_tab_id, :metric_id, :position
+  validates_presence_of :position
   validates_numericality_of :position, :only_integer => true, :greater_than_or_equal_to => 0
 end
