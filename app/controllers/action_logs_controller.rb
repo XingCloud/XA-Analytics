3cbb15ac72c30a0fec79f964890694595637e949
方建 <superman601@126.com>
2013-01-08 17:30:21 +0800

    set action_logs order

--- app/controllers/action_logs_controller.rb
+++ app/controllers/action_logs_controller.rb
@@ -3,7 +3,7 @@ class ActionLogsController < ProjectBaseController
     max_page = (@project.action_logs.length / 10.0).ceil
     params[:page] = 1 unless params[:page].present?
     if params[:page].to_i > 0
-      logs = @project.action_logs.paginate(:page => params[:page], :per_page => 10)
+      logs = @project.action_logs.paginate(:page => params[:page], :per_page => 10, :order => "perform_at DESC")
       render :json => {:action_logs => logs.map(&:attributes), :max_page => max_page}
     else
       render :json => {:action_logs => [], :max_page => max_page}, :status => 400
