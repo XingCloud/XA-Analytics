654be5bb7551286018b55a58190c0f9261b693a2
方建 <superman601@126.com>
2013-04-28 16:44:08 +0800

    create symlink for resque_schedule

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ task :custom_symlink do
     run "cd #{release_path} && rm -rf #{dir} && ln -sf #{shared_path}/#{dir} ."
   end
   
-  %w(database app_config private_pub private_pub_thin).each do |config_file|
+  %w(database app_config private_pub private_pub_thin resque_schedule).each do |config_file|
     run "cd #{release_path} && rm -rf config/#{config_file}.yml && ln -sf #{shared_path}/config/#{config_file}.yml config/#{config_file}.yml"
   end
   
@@ -79,4 +79,5 @@ end
 
 before "deploy:finalize_update", "custom_symlink"
 after "deploy:restart", "resque:restart"
+after "deploy:restart", "resque:scheduler:restart"
 after "deploy:restart", "private_pub:restart"
