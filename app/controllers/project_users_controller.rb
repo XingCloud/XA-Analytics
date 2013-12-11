2c487f71fdad60a2e4faae901d83adbb0700d667
wangchangli <changliwang2009@gmail.com>
2013-12-11 10:55:20 +0800

    release priviledge on project users

--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -5,7 +5,7 @@ class ProjectUsersController < ProjectBaseController
     project_login_user =  @login_user.project_users.find_by_project_id(params[:project_id])
 
     # 项目非外来用户可以获取该项目的所有用户，管理员可以获取任意项目的所有用户
-    if @login_user.role == "admin" or (not project_login_user.nil? and project_login_user.role != "mgriant")
+    if true or @login_user.role == "admin" or (not project_login_user.nil? and project_login_user.role != "mgriant")
       render :json => @project.project_users.map(&:js_attributes) # return project_users instead of users for historical issue
     else
       render :json=>{}, :status=>403
@@ -21,7 +21,7 @@ class ProjectUsersController < ProjectBaseController
     project_login_user =  @login_user.project_users.find_by_project_id(params[:project_id])
 
     # 项目管理员可以修改项目成员在该项目的报告权限，管理员可以修改所有用户在ta所在项目的报告权限
-    if @login_user.role == "admin" or (not project_login_user.nil? and project_login_user.role == "admin")
+    if true or @login_user.role == "admin" or (not project_login_user.nil? and project_login_user.role == "admin")
       ProjectUser.transaction do
         begin
           project_user.update_attributes!(params[:project_user])
@@ -83,7 +83,8 @@ class ProjectUsersController < ProjectBaseController
 
 private
   def find_login_user
-    @login_user = current_user
+    @login_user = User.find_by_name(session[:cas_user])
+    #@login_user = current_user
   end
 
   def find_project # project we are dealing with
